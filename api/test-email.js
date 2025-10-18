59c3f3408e1e6c785666a0f6c1dbbe965b3bbba0
Nylonstrip <nylonstrip@gmail.com>
2025-10-18 13:59:36 -0400

    adjusted test-email.js 1

--- api/test-email.js
+++ api/test-email.js
@@ -1,10 +1,3 @@
-const { generateEmailTemplate } = require('../services/emailTemplate');
-
 export default async function handler(req, res) {
-  const sampleVin = '1HGCM82633A004352';
-  const sampleLink = 'https://carsaavy.com/reports/sample-report.pdf';
-  const html = generateEmailTemplate(sampleVin, sampleLink, false, sampleLink);
-
-  res.setHeader('Content-Type', 'text/html');
-  res.status(200).send(html);
+  res.status(200).send("<h1>CarSaavy Email Test: API works ✅</h1>");
 }
